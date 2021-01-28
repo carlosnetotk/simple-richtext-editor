ec7028fba23d9fecd87f488fe368051cf1d4550e
Carlos Neto <tekoh.mia3@gmail.com>
2021-01-28 12:14:39 -0300

    added the editable field

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container } from './styles/AppStyle';
+import GlobalStyle from './styles/GlobalStyle';
+import { Editor, EditorState } from 'draft-js';
+
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+
+  const [editState, setEditState] = useState<EditorState>(EditorState.createEmpty())
+  const onChange = (editorState: EditorState) => {
+    setEditState(editorState);
+  }
+
+  return (
+    <>
+      <GlobalStyle />
+      <Container>
+        <Editor editorState={editState} onChange={onChange} />
+      </Container>
+    </>
   );
 }
 
